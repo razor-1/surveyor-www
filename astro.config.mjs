318a001ebb446a11c7f1f18b40089d85461aad39
jon <snyder.jon@gmail.com>
2026-05-07 12:03:50 -0700

    feat: seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://codesift.ai',
   output: 'static',
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
 });
